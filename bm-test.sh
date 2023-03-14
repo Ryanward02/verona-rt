13c662a3ae279f4a42555689d8cfefbcecfc73ac
Ryanward02 <ryanward02@gmail.com>
2023-03-14 11:09:34 +0000

    feat: [rlw20] - Implemented a "spin" function and when(*cownlist[])

--- bm-test.sh
+++ bm-test.sh
@@ -12,18 +12,18 @@ cmake .. -GNinja -DCMAKE_BUILD_TYPE=Debug
 ninja
 
 # We can then call each benchmark from the build folder, since ninja has created executables for us.
-# cd ../build/test
 
-# ./bm-con-lotsofcowns
-# ./bm-con-lotsofwhens
-# ./bm-con-servicetime
+./test/bm-con-lotsofcowns
+# ./test/bm-con-lotsofwhens
+# ./test/bm-con-servicetime
 
 # Or we can run the test with ctest
-cd test
-ctest -R bm-con-lotsofcowns
-ctest -R bm-con-lotsofwhens
-ctest -R bm-con-servicetime
+# cd test
+# ctest -R bm-con-lotsofcowns
+# ctest -R bm-con-lotsofwhens
+# ctest -R bm-con-servicetime
 
 # Variable no of cowns: when(cown**, count) - open pr in verona
 # Spinning for certain amount of time inside behaviours - Check chrono api
 # Behaviour Latency: Timestamp at schedule, timestamp at completion of behaviour.
+
--- test/bm/lotsofcowns/lotsofcowns.cc
+++ test/bm/lotsofcowns/lotsofcowns.cc
@@ -4,6 +4,10 @@
 #include <snmalloc/snmalloc.h>
 #include <verona.h>
 #include <debug/harness.h>
+#include <iostream>
+#include <ctime>
+#include <ratio>
+#include <chrono>
 
 
 /**
@@ -15,6 +19,7 @@
 
 class cown
 {
+  // destructor. Not sure when this is called.
 public:
   ~cown()
   {
@@ -23,75 +28,91 @@ public:
 };
 
 using namespace verona::cpp;
+auto sub_array(cown_ptr<cown> list[], int count) {
+  if (count > (sizeof(list)/sizeof(list[0]))) {
+    return list;
+  }
+  
+  cown_ptr<cown> new_list[count];
+  for (int i = 0; i < count; i++) {
+    new_list[i] = list[i];
+  }
+
+  return new_list;
+
+}
+
+auto spin(double seconds) {
+  high_resolution_clock::time_point t1 = high_resolution_clock::now();
+  duration<double> timespan = duration_cast<duration<double>>(t1 - t1);
+
+  while (timespan.count() < seconds) {
+    high_resolution_clock::time_point t2 = high_resolution_clock::now();
+    timespan = duration_cast<duration<double>>(t2 - t1);
+  }
+
+}
+
+using namespace verona::cpp;
 void test_body()
 {
   // Get time here
-  cown_ptr<cown> c1  = make_cown<cown>();
-  cown_ptr<cown> c2  = make_cown<cown>();
-  cown_ptr<cown> c3  = make_cown<cown>();
-  cown_ptr<cown> c4  = make_cown<cown>();
-  cown_ptr<cown> c5  = make_cown<cown>();
-  cown_ptr<cown> c6  = make_cown<cown>();
-  cown_ptr<cown> c7  = make_cown<cown>();
-  cown_ptr<cown> c8  = make_cown<cown>();
-  cown_ptr<cown> c9  = make_cown<cown>();
-  cown_ptr<cown> c10 = make_cown<cown>();
-  cown_ptr<cown> c11 = make_cown<cown>();
-  cown_ptr<cown> c12 = make_cown<cown>();
-  cown_ptr<cown> c13 = make_cown<cown>();
-  cown_ptr<cown> c14 = make_cown<cown>();
-  cown_ptr<cown> c15 = make_cown<cown>();
-  cown_ptr<cown> c16 = make_cown<cown>();
-  cown_ptr<cown> c17 = make_cown<cown>();
-  cown_ptr<cown> c18 = make_cown<cown>();
-  cown_ptr<cown> c19 = make_cown<cown>();
-  cown_ptr<cown> c20 = make_cown<cown>();
+
+  cown_ptr<cown> cowns[1000];
+  for (int i = 0; i < 1000; i++) {
+    cowns[i] = make_cown<cown>();
+  }
+
+  /** 
+   * Created implementations for "lists" of cowns to be passed. 
+   * When accepts a variable argument count, so we can provide a pointer to an array instead, 
+   * which provides us with our arguments.
+  */ 
+
+  high_resolution_clock::time_point t1 = high_resolution_clock::now();
+  when(*cowns) << [t1](auto) 
+  { 
+    // WORKING: implementation of a 5 second spin in the while.
+    high_resolution_clock::time_point t2 = high_resolution_clock::now();
+    
+    spin(2.5);
+
+    t2 = high_resolution_clock::now();
+    duration<double> total = duration_cast<duration<double>>(t2 - t1);
+
+    // Time from schedule to completion of behaviour
+    std::cout << "total time taken:\t" << total.count() << std::endl;
+  };
+  
+  when(*sub_array(cowns, 400)) << [](auto) { Logging::cout() << "log" << Logging::endl; };
 
   // Get time here. This will tell us the time it takes to create lots of cowns
-  when(c1)  << [](auto) { Logging::cout() << "log" << Logging::endl; };
-  when(c2)  << [](auto) { Logging::cout() << "log" << Logging::endl; };
-  when(c3)  << [](auto) { Logging::cout() << "log" << Logging::endl; };
-  when(c4)  << [](auto) { Logging::cout() << "log" << Logging::endl; };
-  when(c5)  << [](auto) { Logging::cout() << "log" << Logging::endl; };
-  when(c6)  << [](auto) { Logging::cout() << "log" << Logging::endl; };
-  when(c7)  << [](auto) { Logging::cout() << "log" << Logging::endl; };
-  when(c7)  << [](auto) { Logging::cout() << "log" << Logging::endl; };
-  when(c8)  << [](auto) { Logging::cout() << "log" << Logging::endl; };
-  when(c9)  << [](auto) { Logging::cout() << "log" << Logging::endl; };
-  when(c10) << [](auto) { Logging::cout() << "log" << Logging::endl; };
-  when(c11) << [](auto) { Logging::cout() << "log" << Logging::endl; };
-  when(c12) << [](auto) { Logging::cout() << "log" << Logging::endl; };
-  when(c13) << [](auto) { Logging::cout() << "log" << Logging::endl; };
-  when(c14) << [](auto) { Logging::cout() << "log" << Logging::endl; };
-  when(c15) << [](auto) { Logging::cout() << "log" << Logging::endl; };
-  when(c16) << [](auto) { Logging::cout() << "log" << Logging::endl; };
-  when(c17) << [](auto) { Logging::cout() << "log" << Logging::endl; };
-  when(c18) << [](auto) { Logging::cout() << "log" << Logging::endl; };
-  when(c19) << [](auto) { Logging::cout() << "log" << Logging::endl; };
-  when(c20) << [](auto) { Logging::cout() << "log" << Logging::endl; };
+  
 
 
   // Get time here. 
-  when(c1, c2, c3, c4, c5) << [](auto, auto, auto, auto, auto) 
-  {
-    // and here. Will tell us when this block begins executing (and therefore how long 5/20 of the last block of whens took.)
-    Logging::cout() << "log" << Logging::endl; 
-  };
+  
+  // when(c1, c2, c3, c4, c5) << [](auto, auto, auto, auto, auto) 
+  // {
+  //   // and here. Will tell us when this block begins executing (and therefore how long 5/20 of the last block of whens took.)
+  //   Logging::cout() << "log" << Logging::endl; 
+  // };
 
-  // then here. Tells us how the rt handles an overlap of cowns.
-  when(c3, c4, c5, c6, c7) << [](auto, auto, auto, auto, auto) 
-  {
-    // and here
-    Logging::cout() << "log" << Logging::endl; 
-  };
+  // // then here. Tells us how the rt handles an overlap of cowns.
+  // when(c3, c4, c5, c6, c7) << [](auto, auto, auto, auto, auto) 
+  // {
+  //   // and here
+  //   Logging::cout() << "log" << Logging::endl; 
+  // };
 
-  // then here. Should only execute after everything else has completed.
-  when(c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15, c16, c17, c18, c19, c20) << 
-    [](auto, auto, auto, auto, auto, auto, auto, auto, auto, auto, auto, auto, auto, auto, auto, auto, auto, auto, auto, auto) 
-  {
-    // and here. Will tell us how long all the above took to execute.
-    Logging::cout() << "log" << Logging::endl;
-  };
+  // // then here. Should only execute after everything else has completed.
+  // when(c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15, c16, c17, c18, c19, c20) << 
+  //   [](auto, auto, auto, auto, auto, auto, auto, auto, auto, auto, auto, auto, auto, auto, auto, auto, auto, auto, auto, auto) 
+  // {
+  //   // and here. Will tell us how long all the above took to execute.
+  //   Logging::cout() << "log" << Logging::endl;
+  // };
+  
 }
 
 
