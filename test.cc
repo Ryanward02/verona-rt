7df037da433582da7bbdbf31c28c457bdc1cfd5d
Ryanward02 <ryanward02@gmail.com>
2023-04-07 21:18:42 +0100

    feat: rlw20 - Added test file to test distribtion generator

--- test.cc
+++ test.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stdlib.h>
 #include <iostream>
+#include "./test/bm/lotsofcowns/zipfDist.h"
 #include <ctime>
 #include <ratio>
 #include <chrono>
@@ -9,39 +10,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-std::string graphout(double zipflist[]) {
-    std::string s = "";
-
-    for (int i = 0; i < 100; i++) {
-
-        s = s + std::to_string(zipflist[i]) + " ";
-    }
-    return s;
-}
-
-double H(int N, int s) {
-  // if no skew is provided, assume a uniform distribution.
-  double ret = 0;
-  for (int i = 1; i <= N; i++) {
-    ret += 1 / pow(i, s);
-  }
-  return ret;
-}
-
-double zipf(int x, int N, double s) {
-  return ((1 / pow(x, s)) / H(N, s));
-}
 
 int main(int argc, char **argv) {
-    double zipflist[100];
-    for (int i = 0; i < 100; i++) {
-        zipflist[i] = zipf(100 - i, 100, 0.01);
-    }
-    for (int i = 0; i < 100; i++) {
-        std::cout << zipflist[i] << std::endl;
+    std::cout << "(1)" << std::endl;
+    ScrambledZipfianGenerator gen = ScrambledZipfianGenerator(0, 100);
+    int indexList[10000];
+
+    for (int i = 0; i < 10000; i++) {
+      int v = gen.nextValue();
+      indexList[i] = v;
     }
 
-    std::string bashCall = "python3 /Users/ryanward/Documents/git_repos/verona-rt/graphOut.py " + graphout(zipflist);
-    system(bashCall.c_str());
+    std::qsort(indexList,sizeof(indexList)/sizeof(*indexList),sizeof(*indexList), cmpInt);
+
+    for (int i = 0; i < 10000; i++) {
+      std::cout << indexList[i] << "  ";
+    }
+    std::cout << std::endl;
+    // std::string bashCall = "python3 /Users/ryanward/Documents/git_repos/verona-rt/graphOut.py " + graphout(zipflist);
+    // system(bashCall.c_str());
+
+    return 0;
 }
 
